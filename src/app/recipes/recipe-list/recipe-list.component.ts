c5619dca20d2617efdc4cfea5a734a9ff2d9fc0a
greg <gdusart@gmail.com>
2017-06-03 16:20:56 +0200

    First commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe('A test recipe', 'This is a test recipe',
+      'http://recettes-de-chefs.ca/images/archives/les_recettes_david_biron/main/00_Beauty_Shot_Cotelettes-large.jpg'),
+    new Recipe('A test recipe', 'This is a test recipe',
+      'http://recettes-de-chefs.ca/images/archives/les_recettes_david_biron/main/00_Beauty_Shot_Cotelettes-large.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
