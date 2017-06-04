1f9908d3ce31d24210a869b23ad421477ccc3d68
greg <gdusart@gmail.com>
2017-06-04 16:49:18 +0200

    more services

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,20 +1,34 @@
-import { EventEmitter } from '@angular/core';
+import { ShoppingListService } from './../shopping-list/shopping-list.service';
+import { Ingredient } from './../shared/ingredient.model';
+import { EventEmitter, Injectable } from '@angular/core';
 
 import { Recipe } from './recipe.model';
 
+@Injectable()
 export class RecipeService {
 
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A test recipe', 'This is a test recipe',
-            'http://recettes-de-chefs.ca/images/archives/les_recettes_david_biron/main/00_Beauty_Shot_Cotelettes-large.jpg'),
-        new Recipe('Another test recipe', 'This is a test recipe',
-            'http://recettes-de-chefs.ca/images/archives/les_recettes_david_biron/main/00_Beauty_Shot_Cotelettes-large.jpg')
+        new Recipe('Paté gaumais', 'Paté gaumais - description',
+            'https://upload.wikimedia.org/wikipedia/commons/thumb/4/42/P%C3%A2t%C3%A9_gaumois_familial.jpg/280px-P%C3%A2t%C3%A9_gaumois_familial.jpg',
+            [new Ingredient('ingredient 1', 5),
+            new Ingredient('ingredient 2', 42)]),
+        new Recipe('Touffaye', 'Touffaye - description',
+            'https://ds1.static.rtbf.be/article/image/624x351/7/9/3/9dc372713683fd865d366d5d9ee810ba-1397820935.jpg',
+            [new Ingredient('ingredient 3', 7)])
     ];
+
+    constructor(private slService: ShoppingListService) {
+    }
+
 
     getRecipes() {
         // defensive copy
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
