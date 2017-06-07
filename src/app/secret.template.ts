b73a68ccba4a463ab2f5ef972d22db7a8d01247d
greg <gdusart@gmail.com>
2017-06-07 21:04:33 +0200

    add template secret file

---
+++ src/app/secret.template.ts
@@ -0,0 +1,7 @@
+// Fill with your firebase credentials and rename this file to secret.ts
+export class Secrets {
+    public static FIREBASE_INIT = {
+        apiKey: '',
+        authDomain: ''
+    };
+}
