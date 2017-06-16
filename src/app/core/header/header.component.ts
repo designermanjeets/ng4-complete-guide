75bd38ca9b72031b8e51cbe30bce35c75d3f6501
greg <gdusart@gmail.com>
2017-06-16 19:15:21 +0200

    Core module definition

---
+++ src/app/core/header/header.component.ts
@@ -0,0 +1,30 @@
+import { Router } from '@angular/router';
+import { AuthService } from '../../auth/auth-service';
+import { DataStorageService } from '../../shared/data-storage.service';
+import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+@Component({
+    selector: 'app-header',
+    templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService, private router: Router) { }
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes().subscribe(
+            (response: Response) => {
+                console.log(response);
+            }
+        );
+    }
+
+    onFetchData() {
+        this.dataStorageService.loadRecipes();
+    }
+
+    onLogout() {
+        this.authService.logout();
+        this.router.navigate(['/']);
+    }
+}
