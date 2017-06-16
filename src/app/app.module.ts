c4542a5b89f16fcab78d08894d909b45fb17adf6
greg <gdusart@gmail.com>
2017-06-16 19:15:05 +0200

    Core module definition

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,20 @@
-import { AuthModule } from './auth/auth.module';
-import { HttpModule } from '@angular/http';
+import {HttpModule} from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 
-import { SharedModule } from './shared/shared.module';
-import { AuthGuard } from './auth/auth-guard.service';
-import { AuthService } from './auth/auth-service';
-import { DataStorageService } from './shared/data-storage.service';
-import { RecipeService } from './recipes/recipe.service';
-import { AppRoutingModule } from './app-routing-module';
+import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
+import {AuthModule} from './auth/auth.module';
+import {AppRoutingModule} from './app-routing-module';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { HomeComponent } from './home/home.component';
+import {AppComponent} from './app.component';
+import {ShoppingListModule} from './shopping-list/shopping-list.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -28,14 +22,8 @@ import { HomeComponent } from './home/home.component';
     AppRoutingModule,
     SharedModule,
     ShoppingListModule,
-    AuthModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    DataStorageService,
-    AuthService,
-    AuthGuard
+    AuthModule,
+    CoreModule
   ],
   bootstrap: [AppComponent]
 })
